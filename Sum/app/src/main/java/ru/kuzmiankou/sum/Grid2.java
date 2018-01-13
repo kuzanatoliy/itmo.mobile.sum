ac68286c917bbe54d51a9ce34f979b64c3015ec1
Kuzmiankou Anatoli <njkznu@gmail.com>
2018-01-13 20:32:46 +0300

    Fix change for project

---
+++ Sum/app/src/main/java/ru/kuzmiankou/sum/Grid2.java
@@ -0,0 +1,13 @@
+package ru.kuzmiankou.sum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Grid2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid2);
+    }
+}
